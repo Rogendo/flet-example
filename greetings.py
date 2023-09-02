5dafe1869048097fd6fd4e341f2bbda79285bc90
Peter <62094358+Rogendo@users.noreply.github.com>
2023-09-02 18:04:24 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -5,23 +5,11 @@ def main(page:ft.Page):
   page.title="greet app"
   page.theme_ = "dark"
   
-  first_name=ft.TextField(place_holder="First name", autofocus=True)
-  last_name=ft.TextFIeld(place_holder="las name", autofocus= True)
-  greet_message="hello {}.value {}.value !"
-  
-  def click_btn(e):
-    
+  first_name=ft.TextField(label="First name", autofocus=True)
+  last_name=ft.TextFIeld(label="last name", autofocus= True)
+  greetings = ft.Column()
 
-    
-  greet_button=ft.ElevatedButton(text="Greet", onClick=click_btn)
-  
-def main(page):
-
-    first_name = ft.TextField(label="First name", autofocus=True)
-    last_name = ft.TextField(label="Last name")
-    greetings = ft.Column()
-
-    def btn_click(e):
+      def btn_click(e):
         greetings.controls.append(ft.Text(f"Hello, {first_name.value} {last_name.value}!"))
         first_name.value = ""
         last_name.value = ""
