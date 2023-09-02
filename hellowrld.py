ff7b61ade9bf9097641bc6eda1b57c6e8fa5e4b5
Peter <62094358+Rogendo@users.noreply.github.com>
2023-09-02 17:25:58 +0300

    Create hellowrld.py

---
+++ hellowrld.py
@@ -0,0 +1,13 @@
+#flet hello world app
+import flet as ft
+from flet import Text, TextField
+from flet import *
+
+def main(page :ft.Page):
+  page.title = "hello world app"
+  page.theme_ = "dark"
+
+  message = ft.Text(value= "hello guys, this us my first flutter app")
+  page.add(message)
+
+flet.app(terget=main)
